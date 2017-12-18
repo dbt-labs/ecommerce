5235ee7fa662c33a81e5ba0857b18bb8c80c40fd
Erica Louie <erica.louie94@gmail.com>
2017-12-18 14:52:52 -0500

    add first_order_date field

--- models/transform/customers.sql
+++ models/transform/customers.sql
@@ -1,1 +1,30 @@
-select * from {{var('customers_table')}}
+with customers as (
+
+    select * from {{var('customers_table')}}
+
+),
+
+orders as (
+
+    select * from {{ref('orders')}}
+
+),
+
+joined as (
+
+    select
+        customers.*,
+        orders.order_number,
+        orders.created_at::date as first_order_date
+    from customers
+    left join orders using (customer_id)
+
+),
+
+first_orders as (
+
+    select * from joined
+    where order_number = 1
+)
+
+select * from first_orders
