8059171744958776a42f359aa15c181d02bc96b3
fred <fshaykis@gmail.com>
2018-04-10 11:57:11 -0400

    exclude calc fields

--- models/orders_calculations.sql
+++ models/orders_calculations.sql
@@ -1,10 +1,3 @@
-{{
-    config(
-        materialized='table',
-        sort='created_at',
-        dist='order_id'
-    )
-}}
 
 {% set order_seq_number = "row_number() over (partition by email order by created_at)" %}
 
@@ -24,9 +17,11 @@ fields as (
         split_part(split_part(created_at, ' ' , 1),'-',3) as order_day_of_month,
 
         case
-            when financial_status ilike 'paid' then 1
+            when financial_status ilike 'paid' and
+                cancelled_at is not null
+                then 1
             else 0
-        end as paid_order,
+        end as completed_order,
 
         case
             when {{ order_seq_number }} = 1
@@ -52,19 +47,19 @@ order_numbers as (
         {{ order_seq_number }} as order_seq_number,
 
         case
-            when paid_order = 1
+            when completed_order = 1
                 then row_number() over (
-                    partition by email, paid_order
+                    partition by email, completed_order
                     order by created_at)
             else null
-        end as paid_order_number
+        end as completed_order_number
 
     from fields
 ),
 
 --the general idea for the next 2 CTEs is:
---calculation_1 calculates fields using case when, making the field null if order_paid = 0,
---calculation_2 calculates values for rows where paid_order = 0 using coalesce with window functions
+--calculation_1 calculates fields using case when, null if order_completed = 0,
+--calculation_2 calculates values where completed_order = 0 using coalesce
 
 calculation_1 as (
 
@@ -73,31 +68,31 @@ calculation_1 as (
         *,
 
         case
-            when paid_order = 1
+            when completed_order = 1
                 then min(created_at) over (
-                    partition by email, paid_order order by created_at
+                    partition by email, completed_order order by created_at
                     rows between unbounded preceding and unbounded following)
             else null
-        end as first_paid_order_date_calc,
+        end as first_completed_order_date_calc,
 
         case
-            when paid_order_number > 1 then lag(created_at, 1) over (
-                partition by email, paid_order order by created_at)
-            when paid_order_number = 1 then null
+            when completed_order_number > 1 then lag(created_at, 1) over (
+                partition by email, completed_order order by created_at)
+            when completed_order_number = 1 then null
             else null
-        end as previous_paid_order_date_calc,
+        end as previous_completed_order_date_calc,
 
         case
-            when paid_order = 1 then count(*) over (
-                partition by email, paid_order)
+            when completed_order = 1 then count(*) over (
+                partition by email, completed_order)
                 else null
-        end as lifetime_paid_orders_calc,
+        end as lifetime_completed_orders_calc,
 
         case
-            when paid_order = 1 then sum(total_price) over (
-                partition by email, paid_order)
+            when completed_order = 1 then sum(total_price) over (
+                partition by email, completed_order)
                 else null
-        end as lifetime_paid_revenue_calc
+        end as lifetime_revenue_calc
 
     from order_numbers
 
@@ -109,8 +104,8 @@ calculation_2 as (
 
         *,
         -- first_order_date and lifetime_placed_orders calculated using all
-            -- orders, regardless of whether order was paid or not.
-        -- Other fields in this CTE are for paid orders only.
+            -- orders, regardless of whether order was completed or not.
+        -- Other fields in this CTE are for completed orders only.
 
         min(created_at) over
             (partition by customer_id
@@ -118,31 +113,31 @@ calculation_2 as (
                     rows between unbounded preceding and unbounded following)
         as first_order_date,
 
-        coalesce(first_paid_order_date_calc,
-            max(first_paid_order_date_calc) over (
+        coalesce(first_completed_order_date_calc,
+            max(first_completed_order_date_calc) over (
                 partition by email order by created_at
                 rows between unbounded preceding and unbounded following))
-        as first_paid_order_date,
+        as first_completed_order_date,
 
         count(*) over (partition by email)
             as lifetime_placed_orders,
 
-        coalesce(lifetime_paid_orders_calc,
-            max(lifetime_paid_orders_calc) over (
+        coalesce(lifetime_completed_orders_calc,
+            max(lifetime_completed_orders_calc) over (
                 partition by email), 0)
-            as lifetime_paid_orders,
+            as lifetime_completed_orders,
 
-        coalesce(lifetime_paid_revenue_calc,
-            max(lifetime_paid_revenue_calc) over (
+        coalesce(lifetime_revenue_calc,
+            max(lifetime_revenue_calc) over (
                 partition by email), 0)
-            as lifetime_paid_revenue,
+            as lifetime_revenue,
 
         case
-            when created_at <= first_paid_order_date_calc then null
-            else coalesce(previous_paid_order_date_calc,
-                lag(previous_paid_order_date_calc, 1) ignore nulls over (
+            when created_at <= first_completed_order_date_calc then null
+            else coalesce(previous_completed_order_date_calc,
+                lag(previous_completed_order_date_calc, 1) ignore nulls over (
                 partition by email order by created_at desc))
-        end as previous_paid_order_date
+        end as previous_completed_order_date
 
     from calculation_1
 ),
@@ -154,28 +149,28 @@ date_diffs as (
         *,
 
         case
-            when created_at < first_paid_order_date then null
-            else datediff(month, first_paid_order_date, created_at)
-        end as months_from_first_paid_order,
+            when created_at < first_completed_order_date then null
+            else datediff(month, first_completed_order_date, created_at)
+        end as months_from_first_completed_order,
 
         case
-            when created_at < first_paid_order_date then null
-            else datediff(week, first_paid_order_date, created_at)
-        end as weeks_from_first_paid_order,
+            when created_at < first_completed_order_date then null
+            else datediff(week, first_completed_order_date, created_at)
+        end as weeks_from_first_completed_order,
 
         case
-            when created_at < first_paid_order_date then null
-            else datediff(day, first_paid_order_date, created_at)
-        end as days_from_first_paid_order,
+            when created_at < first_completed_order_date then null
+            else datediff(day, first_completed_order_date, created_at)
+        end as days_from_first_completed_order,
 
         case
-            when created_at <= first_paid_order_date then null
-            else datediff(day, previous_paid_order_date, created_at)
-        end as days_since_previous_paid_order,
+            when created_at <= first_completed_order_date then null
+            else datediff(day, previous_completed_order_date, created_at)
+        end as days_since_previous_completed_order,
 
         case
-            when created_at < first_paid_order_date then null
-            else datediff(day, first_paid_order_date, current_date)
+            when created_at < first_completed_order_date then null
+            else datediff(day, first_completed_order_date, current_date)
         end as customer_age_days
 
     from calculation_2
@@ -187,21 +182,21 @@ final_calculations as (
 
         *,
 
-        case when lifetime_paid_orders = 1 then 'single_purchaser'
-            when lifetime_paid_orders > 1 then 'repeat_purchaser'
+        case when lifetime_completed_orders = 1 then 'single_purchaser'
+            when lifetime_completed_orders > 1 then 'repeat_purchaser'
             else 'non_purchaser'
         end as customer_type,
 
         sum(case
-                when days_from_first_paid_order <= 30 and paid_order = 1 then 1
+                when days_from_first_completed_order <= 30 and completed_order = 1 then 1
                 else 0
             end) over(
                     partition by email order by created_at
                     rows between unbounded preceding and unbounded following)
-        as customer_first_30_day_paid_orders,
+        as customer_first_30_day_completed_orders,
 
         sum(case
-                when days_from_first_paid_order <= 30 and paid_order = 1
+                when days_from_first_completed_order <= 30 and completed_order = 1
                     then total_price
                 else 0
             end) over(
@@ -210,16 +205,16 @@ final_calculations as (
         as customer_first_30_day_revenue,
 
         sum(case
-                when days_from_first_paid_order <= 60 and paid_order = 1
+                when days_from_first_completed_order <= 60 and completed_order = 1
                     then 1
                 else 0
             end) over (
                     partition by email order by created_at
                     rows between unbounded preceding and unbounded following)
-        as customer_first_60_day_paid_orders,
+        as customer_first_60_day_completed_orders,
 
         sum(case
-                when days_from_first_paid_order <= 60 and paid_order = 1
+                when days_from_first_completed_order <= 60 and completed_order = 1
                     then total_price
                  else 0 end) over(
                      partition by email order by created_at
@@ -227,16 +222,16 @@ final_calculations as (
         as customer_first_60_day_revenue,
 
         sum(case
-                when days_from_first_paid_order <= 90 and paid_order = 1
+                when days_from_first_completed_order <= 90 and completed_order = 1
                     then 1
                 else 0
             end) over
                     (partition by email order by created_at
                     rows between unbounded preceding and unbounded following)
-        as customer_first_90_day_paid_orders,
+        as customer_first_90_day_completed_orders,
 
         sum(case
-                when days_from_first_paid_order <= 90 and paid_order = 1
+                when days_from_first_completed_order <= 90 and completed_order = 1
                     then total_price
                 else 0
             end) over (
---
+++ models/orders_xf.sql
@@ -0,0 +1,23 @@
+-- to exclude extraneous intermediary columns from orders_calculations
+
+{{
+    config(
+        materialized='table',
+        sort='created_at',
+        dist='order_id'
+    )
+}}
+
+select
+{{
+    dbt_utils.star(from=ref('orders_calculations'),
+    except=[
+        "first_completed_order_date_calc",
+        "previous_completed_order_date_calc",
+        "lifetime_completed_orders_calc",
+        "lifetime_revenue_calc"
+           ]
+    )
+}}
+
+from {{ref('orders_calculations')}}
