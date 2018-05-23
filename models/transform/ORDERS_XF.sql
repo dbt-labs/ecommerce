9fe2afaae360a5e5234efc77e49149c8162d5831
fred <fshaykis@gmail.com>
2018-05-23 12:23:00 -0400

    change to except

--- models/transform/ORDERS_XF.sql
+++ models/transform/ORDERS_XF.sql
@@ -2,16 +2,12 @@
 
 {{config(materialized='table')}}
 
-select
-{{
-    dbt_utils.star(from=ref('ORDERS_CALCULATIONS'),
-    except=[
-    "first_completed_order_timestamp_calc",
-    "previous_completed_order_timestamp_calc",
-    "lifetime_completed_orders_calc",
-    "lifetime_revenue_calc"
-           ]
-    )
-}}
+select * except (
+    
+    first_completed_order_timestamp_calc, 
+    previous_completed_order_timestamp_calc,
+    lifetime_completed_orders_calc,
+    lifetime_revenue_calc
+)
 
 from {{ref('ORDERS_CALCULATIONS')}}
