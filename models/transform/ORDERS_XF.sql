89f79d7ea9d1c18e5053a9230361fbc932daa90d
fred <fshaykis@gmail.com>
2018-05-23 12:05:04 -0400

    remove except columns

--- models/transform/ORDERS_XF.sql
+++ models/transform/ORDERS_XF.sql
@@ -6,10 +6,6 @@ select
 {{
     dbt_utils.star(from=ref('ORDERS_CALCULATIONS'),
     except=[
-    "FIRST_COMPLETED_ORDER_TIMESTAMP_CALC",
-    "PREVIOUS_COMPLETED_ORDER_TIMESTAMP_CALC",
-    "LIFETIME_COMPLETED_ORDERS_CALC",
-    "LIFETIME_REVENUE_CALC",
     "first_completed_order_timestamp_calc",
     "previous_completed_order_timestamp_calc",
     "lifetime_completed_orders_calc",
