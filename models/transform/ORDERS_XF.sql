f50e2209161fc81627c2fa6efce09ac45af8224e
fred <fshaykis@gmail.com>
2018-05-23 12:01:24 -0400

    change config of orders_xf

--- models/transform/ORDERS_XF.sql
+++ models/transform/ORDERS_XF.sql
@@ -1,23 +1,17 @@
 -- to exclude extraneous intermediary columns from orders_calculations
 
-{{
-    config(
-        materialized='table',
-        sort='created_at',
-        dist='order_id'
-    )
-}}
+{{config(materialized='table'}}
 
 select
 {{
     dbt_utils.star(from=ref('ORDERS_CALCULATIONS'),
     except=[
-    "FIRST_COMPLETED_ORDER_DATE_CALC",
-    "PREVIOUS_COMPLETED_ORDER_DATE_CALC",
+    "FIRST_COMPLETED_ORDER_TIMESTAMP_CALC",
+    "PREVIOUS_COMPLETED_ORDER_TIMESTAMP_CALC",
     "LIFETIME_COMPLETED_ORDERS_CALC",
     "LIFETIME_REVENUE_CALC",
-    "first_completed_order_date_calc",
-    "previous_completed_order_date_calc",
+    "first_completed_order_timestamp_calc",
+    "previous_completed_order_timestamp_calc",
     "lifetime_completed_orders_calc",
     "lifetime_revenue_calc"
            ]
