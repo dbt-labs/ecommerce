a22e43e8d511cc9aeaaaa2d30c7ed34c6e605d17
fred <fshaykis@gmail.com>
2018-06-01 12:27:48 -0400

    third time's a charm

--- models/transform/customers.sql
+++ models/transform/customers.sql
@@ -14,22 +14,22 @@ joined as (
     select
 
         customers_cte.*,
-        orders.first_order_date,
-        orders.first_completed_order_date,
-        orders.lifetime_placed_orders,
-        orders.lifetime_completed_orders,
-        orders.lifetime_revenue,
-        orders.customer_age_days,
-        orders.customer_type,
-        orders.customer_first_30_day_completed_orders,
-        orders.customer_first_30_day_revenue,
-        orders.customer_first_60_day_completed_orders,
-        orders.customer_first_60_day_revenue,
-        orders.customer_first_90_day_completed_orders,
-        orders.customer_first_90_day_revenue
+        orders_cte.first_order_date,
+        orders_cte.first_completed_order_date,
+        orders_cte.lifetime_placed_orders,
+        orders_cte.lifetime_completed_orders,
+        orders_cte.lifetime_revenue,
+        orders_cte.customer_age_days,
+        orders_cte.customer_type,
+        orders_cte.customer_first_30_day_completed_orders,
+        orders_cte.customer_first_30_day_revenue,
+        orders_cte.customer_first_60_day_completed_orders,
+        orders_cte.customer_first_60_day_revenue,
+        orders_cte.customer_first_90_day_completed_orders,
+        orders_cte.customer_first_90_day_revenue
 
     from customers_cte
-    left join orders_cte as orders using (customer_id)
+    left join orders_cte using (customer_id)
 )
 
 select * from joined
